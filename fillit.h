ff2c7d27754b4a9e9dab24c6ee82d4208d55a03d
Thomas Woerdeman <twoerdem@f1r6s16.codam.nl>
2019-02-07 15:54:58 +0100

    Update fillit.h

--- fillit.h
+++ fillit.h
@@ -6,7 +6,15 @@
 /*   By: twoerdem <twoerdem@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/02/07 15:36:50 by twoerdem       #+#    #+#                */
-/*   Updated: 2019/02/07 15:36:52 by twoerdem      ########   odam.nl         */
+/*   Updated: 2019/02/07 15:54:43 by twoerdem      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FILLIT_H
+# define FILLIT_H
+# define BUFF_SIZE 42
+# include <fcntl.h>
+# include <unistd.h>
+# include "./libft/libft.h"
+
+#endif
