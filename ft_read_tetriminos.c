bde8b46d460bdd125f8dbb73796a31e88c290125
Luc Van vlokhoven <lvan-vlo@f1r6s15.codam.nl>
2019-02-08 15:49:58 +0100

    h

--- ft_read_tetriminos.c
+++ ft_read_tetriminos.c
@@ -1,22 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   fillit.h                                           :+:    :+:            */
+/*   ft_read_tetriminos.c                               :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: lvan-vlo <lvan-vlo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/02/07 15:36:50 by twoerdem       #+#    #+#                */
-/*   Updated: 2019/02/08 13:24:13 by lvan-vlo      ########   odam.nl         */
+/*   Created: 2019/02/08 13:14:36 by lvan-vlo       #+#    #+#                */
+/*   Updated: 2019/02/08 13:22:54 by lvan-vlo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FILLIT_H
-# define FILLIT_H
-# define BUFF_SIZE 42
-# include <fcntl.h>
-# include <unistd.h>
-# include "./libft/libft.h"
+#include "fillit.h"
 
-char	*ft_read_tetriminos(int fd, char *file);
+char	*ft_read_tetriminos(int fd, char *file)
+{
+	char	buf[BUFF_SIZE + 1];
+	char	*tmp;
+	int		ret;
 
-#endif
+	ret = 1;
+	while (ret > 0)
+	{
+		ret = read(fd, buf, BUFF_SIZE);
+		if (ret < 0)
+			return (NULL);
+		buf[ret] = '\0';
+		tmp = ft_strjoin(file, buf);
+		free(file);
+		file = ft_strdup(tmp);
+		free(tmp);
+	}
+	return (file);
+}
--- main.c
+++ main.c
@@ -1,22 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   fillit.h                                           :+:    :+:            */
+/*   main.c                                             :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: lvan-vlo <lvan-vlo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/02/07 15:36:50 by twoerdem       #+#    #+#                */
-/*   Updated: 2019/02/08 13:24:13 by lvan-vlo      ########   odam.nl         */
+/*   Created: 2019/02/07 15:53:12 by lvan-vlo       #+#    #+#                */
+/*   Updated: 2019/02/08 14:43:37 by lvan-vlo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FILLIT_H
-# define FILLIT_H
-# define BUFF_SIZE 42
-# include <fcntl.h>
-# include <unistd.h>
-# include "./libft/libft.h"
+#include "fillit.h"
 
-char	*ft_read_tetriminos(int fd, char *file);
+int		main(int argc, char **argv)
+{
+	char	*file;
+	int		fd;
 
-#endif
+	file = ft_strnew(1);
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		file = ft_read_tetriminos(fd, file);
+		if (file == NULL)
+			return (0);
+//		ft_putendl(file);
+		if (!(ft_check_file(file)))
+		{
+			ft_putendl("Error, invalid input!");
+			return (0);
+		}
+	}
+	return (0);
+}
